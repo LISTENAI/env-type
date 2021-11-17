e53bd559ff689afae236047a60afbfc436369319
xychen <xychen@listenai.com>
2021-11-17 14:50:36 +0800

    Add Flasher API

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,15 @@ declare namespace bundle {
     interface Bundle {
         binaries: string[];
         env: Record<string, string>;
+        flasher?: Flasher;
+    }
+
+    interface Flasher {
+        makeFlashExecArgs(partitions: Record<number, string>, otherArgs?: string[]): FlasherArgs;
+    }
+
+    interface FlasherArgs {
+        command: string;
+        args: string[];
     }
 }
